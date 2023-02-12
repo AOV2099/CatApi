9a68ad95af83f4bd11ccda990e0ad49abb8e1b2f
Moisés Osg <aov2099@gmail.com>
2023-02-12 17:17:11 -0600

    Initial commit

---
+++ app/src/main/java/com/aov2099/catapi/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aov2099.catapi.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aov2099.catapi.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
